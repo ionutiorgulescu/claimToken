88d83a8603dc725b1150225cf858dc142da8b538
Dragos Rizescu <rizedr@gmail.com>
2018-03-02 13:02:55 +0200

    Use Portal w/ mountNode

--- src/App.js
+++ src/App.js
@@ -5,12 +5,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.elementNode = null;
+  }
+
+  componentDidMount() {
+    // this is a hack and should not be done
+    // the reason I am doing it is to display the Portal at elementNode
+    // the correct way would be to use context and another component
+    window.setTimeout(() => {
+      this.forceUpdate();
+    }, 500);
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <div ref={(node) => { this.elementNode = node }} />
         </header>
         <div className="App-wrap">
           <p className="App-intro">
@@ -37,6 +53,14 @@ class App extends Component {
             What's up! What's up! BITCOOOOOOOONEEEECT!
           </Card>
         </Portal>
+
+        { this.elementNode !== null && (
+          <Portal mountNode={this.elementNode}>
+            <p style={{ color: 'red' }}>
+              I should not show here!
+            </p>
+          </Portal>
+        ) }
       </div>
     );
   }
